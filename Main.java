752dbaa5cfa8d37806eff3527cb628d37867941c
prostotema1 <90394799+prostotema1@users.noreply.github.com>
2022-10-28 19:43:36 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
-class Main{
-  public static void main(String [] args){
-    System.out.println("Hello world!");
-  }
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ээаядна эыуо кймжаяйэынажчиймнч мдзэйжйэ: ");
+        var sequence = scanner.nextLine().split("");
+        int [] digits = new int[sequence.length];
+        for(int i =0; i < sequence.length;i++){
+            digits[i] =Integer.parseInt(sequence[i]);
+        }
+        System.out.print("Яявбёпв обяёа: ");
+        int slide = scanner.nextInt();
+        System.out.println();
+        for (int i =0; i < digits.length;i++){
+            var temp = (digits[i] - (slide % 10)) % 10;
+            if(temp < 0){
+                temp += 10;
+            }
+            digits[i] = temp;
+            System.out.print(digits[i]);
+        }
+
+    }
 }
